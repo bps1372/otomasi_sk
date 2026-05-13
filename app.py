3b53acd18b1ae6abee10e1a8f00ed55feb7ee9bc
bps1372 <asistenprakombps1372.maganghub@gmail.com>
2026-05-13 13:50:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,4 @@ st.set_page_config(page_title="GESIT - BPS1372", layout="centered")
 
 st.title("📝 GESIT (Generate SK Instan)")
 st.write("Aplikasi ini akan mengotomasi pengisian dokumen SK Kuasa Pengguna Anggaran BPS Kota Solok.")
-st.write("Website beralih kesini: https://gesit-bps1372.streamlit.app/ (sementara)")
-st.write("Website dalam Proses Pengembangan lebih lanjut dan maintenance")
+st.write("Website beralih kesini: https://gesit-bps1372.streamlit.app/ (sementara dalam proses perbaikan maintenance)")
